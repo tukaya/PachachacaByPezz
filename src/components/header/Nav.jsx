72b50abf864a6dcfb31784522517af9a83007cba
zek0ooo <jaguar92sy@gmail.com>
2020-07-01 21:44:12 +0100

    slid

--- src/components/header/Nav.jsx
+++ src/components/header/Nav.jsx
@@ -7,13 +7,13 @@ import { Link } from 'react-router-dom'
     return (
         <div>
             <ul className='ulN'>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/blog'>Blog</Link></li>
-                <li><Link to='/about'>About</Link></li>
-                <li><Link to='/vision-mission'>Vision & Mission</Link></li>
-                <li><Link to='/albums'>Albums</Link></li>
-                <li><Link to='/contact'>Contact</Link></li>
-                <li><Link to='/donation'>Donation</Link></li>
+                <li><Link className='ulLN' to='/'>Home</Link></li>
+                <li><Link className='ulLN' to='/blog'>Blog</Link></li>
+                <li><Link className='ulLN' to='/about'>About</Link></li>
+                <li><Link className='ulLN' to='/vision-mission'>Vision & Mission</Link></li>
+                <li><Link className='ulLN' to='/albums'>Albums</Link></li>
+                <li><Link className='ulLN' to='/contact'>Contact</Link></li>
+                <li><Link className='ulLN' to='/donation'>Donation</Link></li>
             </ul>
         </div>
     )
