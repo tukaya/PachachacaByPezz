5474bcaee437b3dd1145eb9ed6fb3c6151a2856e
zek0ooo <jaguar92sy@gmail.com>
2020-07-06 17:45:49 +0100

    new laibrari

--- src/components/sections/blog/Blog.jsx
+++ src/components/sections/blog/Blog.jsx
@@ -3,12 +3,12 @@
 import React from 'react';
 // import { makeStyles } from '@material-ui/core/styles';
 
-import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container';
+// import Grid from '@material-ui/core/Grid';
+// import Container from '@material-ui/core/Container';
 
 // import Header from './Header';
-import MainFeaturedPost from './MainFeaturedPost';
-import FeaturedPost from './FeaturedPost';
+// import MainFeaturedPost from './MainFeaturedPost';
+// import FeaturedPost from './FeaturedPost';
 // import Main from './Main';
 // import Sidebar from './Sidebar';
 // import Footer from './Footer';
@@ -22,72 +22,72 @@ import FeaturedPost from './FeaturedPost';
 //   },
 // }));
 
-const mainFeaturedPost = {
-  title:
-    'Welcome to our blog! Here, you read all about our activities, on-site works and more.',
-  description:
-    'Pacha means Earth, Chaca means Bridge! Pachachaca can therefore be interpreted as a bridge over the world; and a bridge between the children of Peru and the global community.',
-  image: 'https://source.unsplash.com/featured/?{horizon}',
-  imgText: 'main image description',
-  // linkText: 'Continue reading…',
-};
+// const mainFeaturedPost = {
+//   title:
+//     'Welcome to our blog! Here, you read all about our activities, on-site works and more.',
+//   description:
+//     'Pacha means Earth, Chaca means Bridge! Pachachaca can therefore be interpreted as a bridge over the world; and a bridge between the children of Peru and the global community.',
+//   image: 'https://source.unsplash.com/featured/?{horizon}',
+//   imgText: 'main image description',
+//   // linkText: 'Continue reading…',
+// };
 
-const featuredPosts = [
-  {
-    title: 'About Pachachaca',
-    date: 'July 1',
-    description:
-      'Pachachaca is a program  for educational guidance and a community center located in the Andes of Calca, Peru. Pachachaca is a name that combines two words from the Andean culture in the indigenous Quechua  language: Pacha means Earth, Chaca means Bridge.',
-    image: 'https://source.unsplash.com/featured/?{together}',
-    imageText: 'Image Text',
-  },
-  {
-    title: 'Sustainability',
-    date: 'July 1',
-    description:
-      'In 2018, together with the Dutch-Peruvian development organization Por Eso!, we wrote a plan to build an ecologically aware vegetable garden and drying cabinet for use in 2019.',
-    image: 'https://source.unsplash.com/featured/?{community}',
-    imageText: 'Image Text',
-  },
-];
+// const featuredPosts = [
+//   {
+//     title: 'About Pachachaca',
+//     date: 'July 1',
+//     description:
+//       'Pachachaca is a program  for educational guidance and a community center located in the Andes of Calca, Peru. Pachachaca is a name that combines two words from the Andean culture in the indigenous Quechua  language: Pacha means Earth, Chaca means Bridge.',
+//     image: 'https://source.unsplash.com/featured/?{together}',
+//     imageText: 'Image Text',
+//   },
+//   {
+//     title: 'Sustainability',
+//     date: 'July 1',
+//     description:
+//       'In 2018, together with the Dutch-Peruvian development organization Por Eso!, we wrote a plan to build an ecologically aware vegetable garden and drying cabinet for use in 2019.',
+//     image: 'https://source.unsplash.com/featured/?{community}',
+//     imageText: 'Image Text',
+//   },
+// ];
 
 // const posts = [post1, post2, post3];
 
 export default function Blog() {
   // const classes = useStyles();
 
-  return (
-    <React.Fragment >
+  return ( <div></div>
+    // <React.Fragment >
       
-      <Container maxWidth='lg' >
-        {/* <Header title='Blog' sections={sections} /> */}
-        <main style={{ paddingTop: '40px' }}>
-          <MainFeaturedPost post={mainFeaturedPost} />
-          <Grid container spacing={4}>
-            {featuredPosts.map((post) => (
-              <FeaturedPost key={post.title} post={post} />
-            ))}
-          </Grid>
-          <Grid container spacing={4}>
-            {featuredPosts.map((post) => (
-              <FeaturedPost key={post.title} post={post} />
-            ))}
-          </Grid>
-          {/* <Grid container spacing={5} className={classes.mainGrid}>
-            <Main title='From the firehose' posts={posts} />
-            <Sidebar
-              title={sidebar.title}
-              description={sidebar.description}
-              archives={sidebar.archives}
-              social={sidebar.social}
-            />
-          </Grid> */}
-        </main>
-      </Container>
-      {/* <Footer
-        title='Footer'
-        description='Something here to give the footer a purpose!'
-      /> */}
-    </React.Fragment>
+    //   <Container maxWidth='lg' >
+    //     {/* <Header title='Blog' sections={sections} /> */}
+    //     <main style={{ paddingTop: '40px' }}>
+    //       <MainFeaturedPost post={mainFeaturedPost} />
+    //       <Grid container spacing={4}>
+    //         {featuredPosts.map((post) => (
+    //           <FeaturedPost key={post.title} post={post} />
+    //         ))}
+    //       </Grid>
+    //       <Grid container spacing={4}>
+    //         {featuredPosts.map((post) => (
+    //           <FeaturedPost key={post.title} post={post} />
+    //         ))}
+    //       </Grid>
+    //       {/* <Grid container spacing={5} className={classes.mainGrid}>
+    //         <Main title='From the firehose' posts={posts} />
+    //         <Sidebar
+    //           title={sidebar.title}
+    //           description={sidebar.description}
+    //           archives={sidebar.archives}
+    //           social={sidebar.social}
+    //         />
+    //       </Grid> */}
+    //     </main>
+    //   </Container>
+    //   {/* <Footer
+    //     title='Footer'
+    //     description='Something here to give the footer a purpose!'
+    //   /> */}
+    // </React.Fragment>
   );
 }
